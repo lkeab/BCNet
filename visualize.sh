b93db356d860d99ece4207a920cb7e577c40e554
lkeab <keleiwhu@gmail.com>
2021-04-12 00:54:21 +0800

    vis

---
+++ visualize.sh
@@ -0,0 +1,10 @@
+python3 setup.py build develop #--no-deps
+export PYTHONPATH=$PYTHONPATH:`pwd`
+export CUDA_LAUNCH_BLOCKING=1 # for debug
+
+CUDA_VISIBLE_DEVICES=0 python3 demo/demo.py --config-file configs/fcos/fcos_imprv_R_101_FPN.yaml \
+  --input 'test_img_selected/' \
+  --output 'result_img_selected/' \
+  --opts MODEL.WEIGHTS ./pretrained_models/model_final_beta.pth
+
+#detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl
